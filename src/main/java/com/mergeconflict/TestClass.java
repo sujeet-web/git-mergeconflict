e7c15e238a5ef6bb30a81cff7f01741fa9edf2af
sujeet-web <sujeetsharma675@gmail.com>
2026-07-21 23:10:52 +0530

    second commit

---
+++ src/main/java/com/mergeconflict/TestClass.java
@@ -0,0 +1,8 @@
+package com.mergeconflict;
+
+public class TestClass {
+
+    public void message(){
+        System.out.println("Hi");
+    }
+}
--- src/main/java/com/mergeconflict/controller/HomeController.java
+++ src/main/java/com/mergeconflict/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.mergeconflict;
+package com.mergeconflict.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
         public String getMessage(){
-            return "Hello";
+            return "Hello!!";
         }
 
 }
