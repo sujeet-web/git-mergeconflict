44a91f3bb9616c6ff8a7d6e2424093ca2d4fe855
sujeet-web <sujeetsharma675@gmail.com>
2026-07-22 00:03:21 +0530

    senth commit

--- src/main/java/com/mergeconflict/TestClass.java
+++ src/main/java/com/mergeconflict/TestClass.java
@@ -8,7 +8,7 @@ public class TestClass {
         System.out.println("Hello");
         System.out.println("Hello Atharv");
         System.out.println("Hello sujeet");
-         System.out.println("Hello sujeet main");
+        System.out.println("Hello sujeet main");
         System.out.println("Hello sujeet main!!!!!!");
         System.out.println("Hello sujeet from feature");
         System.out.println("Hello sujeet from feature!!!!!");
