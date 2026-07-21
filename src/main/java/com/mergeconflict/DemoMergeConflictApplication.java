64d5978f84a44739dea8d7bc2f7bc8c1a0d86be5
sujeet-web <sujeetsharma675@gmail.com>
2026-07-21 22:23:36 +0530

    Initial commit

---
+++ src/main/java/com/mergeconflict/DemoMergeConflictApplication.java
@@ -0,0 +1,13 @@
+package com.mergeconflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMergeConflictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMergeConflictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mergeconflict/DemoMergeConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mergeconflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMergeConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
