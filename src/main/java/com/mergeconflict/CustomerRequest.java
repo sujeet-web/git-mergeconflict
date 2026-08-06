62ed5fb4673bc0c3cf9892f6cb2f8bfc1abd3998
sujeet-web <sujeetsharma675@gmail.com>
2026-08-06 13:10:40 +0530

    new classes added

---
+++ src/main/java/com/mergeconflict/CustomerRequest.java
@@ -0,0 +1,73 @@
+package com.mergeconflict;
+
+public class CustomerRequest {
+
+    private String customerId;;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Integer age;
+
+    public CustomerRequest() {
+    }
+
+    public CustomerRequest(String customerId, String firstName, String lastName,
+                           String email, String phoneNumber, Integer age) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
