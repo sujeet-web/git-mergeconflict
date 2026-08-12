da8a1b119584d21be9dc16e037fd74e99e983505
sujeet-web <72919130+sujeet-web@users.noreply.github.com>
2026-08-12 10:39:49 +0530

    Update CustomerRequest.java

--- src/main/java/com/mergeconflict/CustomerRequest.java
+++ src/main/java/com/mergeconflict/CustomerRequest.java
@@ -3,6 +3,7 @@ package com.mergeconflict;
 public class CustomerRequest {
 
     private String customerId;;
+    private String firstName1;
     private String firstName;
     private String newName;
     private String middleName;
